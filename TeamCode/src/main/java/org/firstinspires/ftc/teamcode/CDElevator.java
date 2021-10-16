d649f5a534a9cdefbf5a2982d45a594e7dd15ce0
LOBrouwerA28 <LOBrouwerA28@stu.lok12.org>
2021-10-16 15:40:48 -0400

    there are 7 unresolved errors in CDElevator.java that i am confused about

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDElevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDElevator.java
@@ -5,53 +5,56 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 
 public class CDElevator {
-
-    CDHardware robotHardware;
-
-    public CDElevator(CDHardware theHardware){
-
-        robotHardware = theHardware;
-
-       // robotHardware.elevatorswitchtop;
-       // robotHardware.elevatorswitchmiddle;
-       // robotHardware.elevatorswitchbottom;
-       // robotHardware.elevatorswitchground;
-
-        robotHardware.elevatormotor.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        robotHardware.elevatormotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    }
-
-    public void setElevatorPower(double pow) {
-        robotHardware.elevatormotor.setPower(pow);
-    }
-
-    // TODO: Add public method to gotoPosition(Top, Middle, bottom)
-
-    //
-    static final double DRIVE_SPEED = 1.0;,
+    static final double DRIVE_SPEED = 1.0;
     static final double ELEVATOR_HOLD_VOLTS = 0.1;
     static final double POS_TOLERANCE = 0.05;
     static final double LOW_POSITION_VOLTS = 3.3;
     static final double PICKUP_POSITION_VOLTS = 3.05;
     static final double MIDDLE_POSITION_VOLTS = 1.15;
     static final double HIGH_POSITION_VOLTS = 0.80;
-    HardwarePlatter theHardwarePlatter;
+    static final double DRIVE_POSITION_VOLTS = 1.15;
+    // static final double theHardware;
+    // ^HardwarePlatter
     private double driveSpeedSetPoint = 0.0;
     private boolean is_moving = false;
     private double targetPosition;
-    public Elevator(HardwarePlatter hwPlatter) {
-        theHardwarePlatter = hwPlatter;
+    //public Elevator(theHardware hwPlatter) {
+    //theHardware = hwPlatter;
+    CDHardware robotHardware;
+
+    public CDElevator(CDHardware theHardware) {
+
+        robotHardware = theHardware;
+
+        // robotHardware.elevatorswitchtop;
+        // robotHardware.elevatorswitchmiddle;
+        // robotHardware.elevatorswitchbottom;
+        // robotHardware.elevatorswitchground;
+
+        robotHardware.elevatormotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        robotHardware.elevatormotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // TODO: Add public method to gotoPosition(Top, Middle, bottom)
+
+        //
+
     }
+
+    public void setElevatorPower(double pow) {
+        robotHardware.elevatormotor.setPower(pow);
+    }
+
+
     private void gotoPosition() {
-        double error = targetPosition - theHardwarePlatter.ElevatorPotentiometer.getVoltage();
+        double error = targetPosition - robotHardware.elevatordistancesensor.getDistance();
         if (Math.abs(error) > POS_TOLERANCE) {
             is_moving = true;
             if (error < 0) {
-                driveSpeedSetPoint = INTAKE_DRIVE_SPEED; //up
+                driveSpeedSetPoint = DRIVE_SPEED; //up
                 driveElevator2();
             } else if (error > 0) {
-                driveSpeedSetPoint = -INTAKE_DRIVE_SPEED * 0.5; //down
+                driveSpeedSetPoint = DRIVE_SPEED * 0.5; //down
                 driveElevator2();
             }
         } else {
@@ -59,44 +62,53 @@ public class CDElevator {
             stop();
         }
     }
+
     void pickUp() {
         targetPosition = PICKUP_POSITION_VOLTS;
         gotoPosition();
     }
+
     void middle() {
         targetPosition = MIDDLE_POSITION_VOLTS;
         gotoPosition();
     }
+
     void low() {
         targetPosition = LOW_POSITION_VOLTS;
         gotoPosition();
     }
+
     void high() {
         targetPosition = HIGH_POSITION_VOLTS;
         gotoPosition();
     }
+
     private void driveElevator() {
-        theHardwarePlatter.ElevatorDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        theHardwarePlatter.ElevatorDrive.setPower(driveSpeedSetPoint);
+        robotHardware.elevatormotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robotHardware.elevatormotor.setPower(driveSpeedSetPoint);
         is_moving = false;
     }
+
     private void driveElevator2() {
-        theHardwarePlatter.ElevatorDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        theHardwarePlatter.ElevatorDrive.setPower(driveSpeedSetPoint);
+        robotHardware.elevatormotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robotHardware.elevatormotor.setPower(driveSpeedSetPoint);
     }
+
     void move(double speed) {
         driveSpeedSetPoint = Math.pow(speed, 3) * 0.5; //was 0.2
         driveElevator();
     }
+
     void stop() {
-        theHardwarePlatter.ElevatorDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robotHardware.elevatormotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         is_moving = false;
-        if (theHardwarePlatter.ElevatorPotentiometer.getVoltage() > DRIVE_POSITION_VOLTS) {
-            theHardwarePlatter.ElevatorDrive.setPower(ELEVATOR_HOLD_VOLTS);
+        if (robotHardware.elevatordistancesensor.getDistance() > DRIVE_POSITION_VOLTS) {
+            robotHardware.elevatormotor.setPower(ELEVATOR_HOLD_VOLTS);
         } else {
-            theHardwarePlatter.ElevatorDrive.setPower(-ELEVATOR_HOLD_VOLTS);
+            robotHardware.elevatormotor.setPower(-ELEVATOR_HOLD_VOLTS);
         }
     }
+
     boolean moveOrHoldPosition() {
         if (is_moving)
             gotoPosition();
@@ -104,10 +116,13 @@ public class CDElevator {
             stop();
         return (is_moving);
     }
+
     boolean isMoving() {
         return (is_moving);
     }
+
     void display(Telemetry telemetry) {
-        telemetry.addData("pot", theHardwarePlatter.ElevatorPotentiometer.getVoltage());
+        telemetry.addData("pot", robotHardware.elevatordistancesensor.getDistance();
         telemetry.addData("isMoving", isMoving());
-    } }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDHardware.java
@@ -21,7 +21,7 @@ public class CDHardware {
     public DcMotor intakemotor;
     public DcMotor duckspinnermotor;
     public DistanceSensor elevatordistancesensor;
-
+    public
 
     public CDHardware (HardwareMap hwMap){
        //Defines Hardware map from Control Hub
